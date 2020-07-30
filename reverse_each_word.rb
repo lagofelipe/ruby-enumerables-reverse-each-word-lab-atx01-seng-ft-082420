ef9e784c41f65fac353717dbf7f5a3d1b5778895
Felipe Dolago <lagofelipe@gmail.com>
2020-07-30 21:15:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
 #iterate thru a string of words ulilizing the each method
- src_array << string.split(" ")
+ src_array = string.split(" ")
 container = []
  src_array.each do |string|
    container << string.reverse
