980d2736d0cdb68002c1b5715dc0edb483157e77
Felipe Dolago <lagofelipe@gmail.com>
2020-07-30 20:20:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(string)
 string = string.split("")
 container = []
 
-string.each do |item|reversed.unshift(item)
+string.each {|item|container.unshift(item)}
          return container.join ("")
 
-end
+
 end
