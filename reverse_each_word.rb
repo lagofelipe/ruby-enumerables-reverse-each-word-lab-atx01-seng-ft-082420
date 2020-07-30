2b405fda27951e726dc4f7ff0feb8fd4fc4a2f40
Felipe Dolago <lagofelipe@gmail.com>
2020-07-30 21:12:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,33 +1,10 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def reverse_each_word(string)
 #iterate thru a string of words ulilizing the each method
 container = []
  src_array << string.split(" ")
  src_array.each do |string|
-   container<< string.reverse
+   container << string.reverse
  end
  container.join(" ")
-
-
-
-
-#returns the same sentence (order) but with each word reversed in place
 
 end
